fa0295e7e5b0f5c033508ce4156f95e97302f399
RKJO <rkjozwiak@gmail.com>
2018-05-13 16:43:13 +0200

    messanger_app_part4

--- messanger_app/models/__init__.py
+++ messanger_app/models/__init__.py
@@ -1,15 +1,17 @@
+from clcrypto import password_hash
+
 """
 CREATE TABLE Users(
 id serial,
 username varchar(255),
 email varchar(255),
-hasher_password varchar(255),
+hashed_password varchar(255),
 PRIMARY KEY (id)
 );
 """
 
 class User:
-    __id = None
+    __id = -1
     username = None
     __hashed_password = None
     email = None
@@ -27,14 +29,21 @@ class User:
             loaded_user.__hashed_password = data[3]
             return loaded_user
 
-    def save_to_db(self):
-        pass
+    def save_to_db(self, cursor):
+        if self.__id == -1:
+            sql = """INSERT INTO Users(username, email, hashed_password)
+                             VALUES(%s, %s, %s) RETURNING id"""
+            cursor.execute(sql, (self.username, self.email, self.__hashed_password))
+            (id,) = cursor.fetchone()
+            self.__id = id
+            #self.__id = cursor.fetchone()[0]  # albo cursor.fetchone()['id']
+            return True
 
     def set_password(self, new_pasword):
-        self.__hashed_password = new_pasword
+        self.__hashed_password = password_hash(new_pasword)
 
     def delete_user(self):
-        pass
+        raise NotImplementedError
 
     @staticmethod
     def get_all_users():
