5a070c72812377c67fc2ef9840ead442b20b5006
RKJO <rkjozwiak@gmail.com>
2018-05-15 15:41:50 +0200

    Product part 2

--- exercise_1_Product.py
+++ exercise_1_Product.py
@@ -6,17 +6,22 @@ class Product:
     price = None
     quantity = None
 
+    def __str__(self):
+         return '%d: %s, %s, %d, %d' % (self.__id, self.name, self.description, self.price, self.quantity)
+
     def __init__(self, n, d, p, q):
-        if isinstance(n, str) and isinstance(d, str)
+        if isinstance(n, str) and isinstance(d, str):
             self.name = n
             self.description = d
         else:
             raise Exception("Zła nazwa lub opis")
+
         if isinstance(p, float) and p >= 0.01:
             self.price = p
         else:
             raise Exception('Zła cena produktu')
-        if isinstance(q, int) and q > 1:
+
+        if isinstance(q, int) and q >= 1:
             self.quantity = q
         else:
             raise Exception('Zła liczność produktu')
@@ -26,8 +31,8 @@ class Product:
 
     def get_total_sum(self):
         total_sum = self.quantity * self.price
-        return total_sum
+        return total_sum if self.quantity < 3 else 0.8 * total_sum
 
-    def get_id(self):
-        return self.next_id
-
+    @property
+    def id_get(self):
+        return self.__id
